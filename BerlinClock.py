4779e593c2a613cb80c4660ab35df8f6ca2752c2
CeddlyBurge <ceddlyburge@gmail.com>
2019-11-07 18:01:01 +0000

    Improve names

--- BerlinClock.py
+++ BerlinClock.py
@@ -1,46 +1,41 @@
-def get_clock(julian_time):
+def berlin_clock_time(julian_time):
     hours, minutes, seconds = list(map(int, julian_time.split(":")))
 
-    single_second = get_seconds(seconds % 2)
-    five_hours = get_five_hours(hours)
-    single_hours = get_hours(hours % 5)
-    five_minutes = get_five_minutes(minutes)
-    single_minutes = get_minutes(minutes % 5)
-
     return [
-		single_second,
-		five_hours,
-		single_hours,
-		five_minutes,
-		single_minutes]
+        seconds_row_lights(seconds % 2)
+        , five_hours_row_lights(hours)
+        , single_hours_row_lights(hours % 5)
+        , five_minutes_row_lights(minutes)
+        , single_minutes_row_lights(minutes % 5)
+    ]
 
 
-def get_seconds(seconds):
+def seconds_row_lights(seconds):
     return ["Y","O"][seconds%2]
 
 
-def get_hours(hours_mod_5):
+def five_hours_row_lights(hours):
+    lights_on = hours // 5
+    lights_in_row = 4
+    return lights_for_row("R", lights_on, lights_in_row)
+
+
+def single_hours_row_lights(hours_mod_5):
     lights_on = hours_mod_5
     lights_in_row = 4
     return lights_for_row("R", lights_on, lights_in_row)
 
 
-def get_five_minutes(minutes):
+def five_minutes_row_lights(minutes):
     lights_on = minutes // 5
     lights_in_row = 11
     return lights_for_row("Y", lights_on, lights_in_row)
 
 
-def get_minutes(minutes_mod_5):
+def single_minutes_row_lights(minutes_mod_5):
     lights_on = minutes_mod_5
     lights_in_row = 4
     return lights_for_row("Y", lights_on, lights_in_row)
-
-
-def get_five_hours(hours):
-    lights_on = hours // 5
-    lights_in_row = 4
-    return lights_for_row("R", lights_on, lights_in_row)
 
 
 def lights_for_row(character, lights_on, lights_in_row):
@@ -49,7 +44,7 @@ def lights_for_row(character, lights_on, lights_in_row):
 
 if __name__ == "__main__":
     time = input()
-    result = get_clock(time)
+    result = berlin_clock_time(time)
     print ("\n".join(result))
 
 
